e48764788f46bf21f16fe668203f29f8e0855dc8
Yongkeun <xdkeun@gmail.com>
2022-08-11 06:02:34 +0900

    feat: kakaoBookAPI 완성

--- kakaoBookAPIs/app.js
+++ kakaoBookAPIs/app.js
@@ -2,11 +2,13 @@ const REST_API_KEY = "93965c2d87d826b6a2c82e922835057c";
     $.ajax({
         method: "GET",
         url: "https://dapi.kakao.com/v3/search/book?target=title",
-        data: { query:"미움받을 용기" },
+        data: { query:"우리" },
         headers: {Authorization: `KakaoAK ${REST_API_KEY}`},
     })
     .done(function( msg ) {
-        console.log(msg.documents[0].title);
-        $( "div" ).append( `<strong>${msg.documents[0].title}</strong>`);
-        $( "div" ).append( `<img src=${msg.documents[0].thumbnail}>`);
+        console.log(msg.documents[2]);
+        $( "div" ).append( `책의 제목 : <strong>${msg.documents[0].title}</strong> <br>`);
+        $( "div" ).append( `책 표지 : <img src=${msg.documents[0].thumbnail} /> <br>`);
+        $( "div" ).append( `책의 출판사 : ${msg.documents[0].publisher} <br>`);
+        $( "div" ).append( `책의 가격 : ${msg.documents[0].price} <br>`);
     });
