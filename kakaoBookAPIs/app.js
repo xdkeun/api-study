c24e7f6e4fe5bff422dbb65beb797a65122a73ef
Yongkeun <xdkeun@gmail.com>
2022-08-13 23:48:42 +0900

    feat: kakao API 수정

--- kakaoBookAPIs/app.js
+++ kakaoBookAPIs/app.js
@@ -1,3 +1,4 @@
+//창업대전 API
 const REST_API_KEY = "93965c2d87d826b6a2c82e922835057c";
     $.ajax({
         method: "GET",
@@ -7,8 +8,9 @@ const REST_API_KEY = "93965c2d87d826b6a2c82e922835057c";
     })
     .done(function( msg ) {
         console.log(msg.documents[2]);
-        $( "div" ).append( `책의 제목 : <strong>${msg.documents[0].title}</strong> <br>`);
-        $( "div" ).append( `책 표지 : <img src=${msg.documents[0].thumbnail} /> <br>`);
-        $( "div" ).append( `책의 출판사 : ${msg.documents[0].publisher} <br>`);
-        $( "div" ).append( `책의 가격 : ${msg.documents[0].price} <br>`);
+        $( "div" ).append( `책의 제목 : <strong>${msg.documents[1].title}</strong> <br>`);
+        $( "div" ).append( `책 표지 : <img src=${msg.documents[1].thumbnail} /> <br>`);
+        $( "div" ).append( `책의 출판사 : ${msg.documents[1].publisher} <br>`);
+        $( "div" ).append( `책의 가격 : ${msg.documents[1].price} <br>`);
+
     });
